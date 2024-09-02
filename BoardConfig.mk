b7bbbfd4b10c123d98942e2d9295bc005099ddb2
bengris32 <bengris32@protonmail.ch>
2024-09-02 15:16:19 +0000

    lyriq: Build VBMeta image with `--set_hashtree_disabled_flag`
    * This is equivalent to building with --flags 1, which just
      disables hashtree verification. This is enough for the bootloader
      to pass "androidboot.vbmeta.device_state=unlocked androidboot.veritymode=disabled"
      in the kernel command line, which is what we wanted this entire time.
    
    * As explained in an earlier commit, any image with flags 3 will cause
      the bootloader to totally break, which is why we must use flags 1
      instead. Besides, flags 1 does exactly what we want it to now.
    
    * PS: MediaTek fix your stupid bootloader
    
    Change-Id: I7b9e5f7181f1b861bb416329ad90725fd6ec2c39
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ VENDOR_SECURITY_PATCH := 2026-01-01
 BOARD_AVB_ENABLE := true
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
