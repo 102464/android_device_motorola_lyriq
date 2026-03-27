acedb303e98bf5a92149d97e33a539fc2d3827d1
Alvyrion <alvyrion0@gmail.com>
2026-03-27 22:08:28 +0530

    lyriq: Initial Skeleton
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# Android Device tree for the Motorola Edge 40 (lyriq)
+
+```
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ device.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/lyriq
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/lyriq/lyriq-vendor.mk)
